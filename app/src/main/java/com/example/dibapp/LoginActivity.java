89eca935251ad4705f260e832d6b1a0f5ca4336e
l.mongelli10 <retro.guitar4698@gmail.com>
2020-03-05 17:45:07 +0100

    Layout lost password

--- app/src/main/java/com/example/dibapp/LoginActivity.java
+++ app/src/main/java/com/example/dibapp/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.dibapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button lost=(Button)findViewById(R.id.recuperapwd);
+        lost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent lostpwd= new Intent(LoginActivity.this, LostPasswordActivity.class);
+                startActivity(lostpwd);
+            }
+        });
     }
 }
