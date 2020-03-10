2336338bebc46e6e7a1b3a9341f9fdac0a038fd8
l.mongelli10 <retro.guitar4698@gmail.com>
2020-03-10 22:31:33 +0100

    Implementata La parte del profilo Utente

--- app/src/main/java/com/example/dibapp/ProfileActivity.java
+++ app/src/main/java/com/example/dibapp/ProfileActivity.java
@@ -2,9 +2,12 @@ package com.example.dibapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -18,7 +21,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -27,37 +33,42 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener{
 
     private AppBarConfiguration mAppBarConfiguration;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
+    private DrawerLayout drawer;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-
-
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
         String uid=user.getUid();
         DatabaseReference db=FirebaseDatabase.getInstance().getReference().child("users").child(uid);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_mycourses);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        View parent = navigationView.getHeaderView(0);
+        Menu navMenu = navigationView.getMenu();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.nav_host_fragment, new MyCourseFragment());
+        ft.commit();
+        toolbar.setTitle(R.string.MyCourses);
 
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-        View headerView=navigationView.getHeaderView(0);
 
         //gestione della schermata relativa all'UTENTE CORRENTE
         db.addValueEventListener(new ValueEventListener() {
@@ -72,7 +83,7 @@ public class ProfileActivity extends AppCompatActivity {
                 if (teacher)  typeText.setText(R.string.teacher);
                 else typeText.setText(R.string.student);
                 //definizione ed implementazione del fab in base al tipo di utente
-                FloatingActionButton fab = findViewById(R.id.fab);
+                fab = findViewById(R.id.fab);
 
                 if (teacher) {
                     fab.setOnClickListener(new View.OnClickListener() {
@@ -84,7 +95,15 @@ public class ProfileActivity extends AppCompatActivity {
                     });
 
                 } else{
-                    fab.hide();
+
+                    fab.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent listCourse=new Intent(ProfileActivity.this, ListCourseActivity.class);
+                            startActivity (listCourse);
+                        }
+                    });
+
                 }
 
 
@@ -113,5 +132,31 @@ public class ProfileActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id =menuItem.getItemId();
+        if (id==R.id.nav_mycourses){
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.nav_host_fragment, new MyCourseFragment());
+            ft.commit();
+            toolbar.setTitle(R.string.MyCourses);
+            fab.show();
+        }
+            if (id==R.id.nav_editProfile){
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.nav_host_fragment, new EditFragment());
+                ft.commit();
+                toolbar.setTitle(R.string.EditProfile);
+                fab.hide();
+        }
+        if (id==R.id.nav_logout){
+            FirebaseAuth.getInstance().signOut();
+            finish();
+            Toast.makeText(ProfileActivity.this, "Successfully Logged Out!", Toast.LENGTH_SHORT).show();
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
