9b2660f4e2cbd238fadf609d29871cd30472e173
l.mongelli10 <retro.guitar4698@gmail.com>
2020-03-10 11:06:39 +0100

    Modifica Edit

--- app/src/main/java/com/example/dibapp/ProfileActivity.java
+++ app/src/main/java/com/example/dibapp/ProfileActivity.java
@@ -1,31 +1,117 @@
 package com.example.dibapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-import androidx.viewpager.widget.ViewPager;
+import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
-import com.example.dibapp.ui.main.SectionsPagerAdapter;
+import androidx.appcompat.widget.Toolbar;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
+        Toolbar toolbar = findViewById(R.id.toolbar);
 
+
+        setSupportActionBar(toolbar);
+        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+        String uid=user.getUid();
+        DatabaseReference db=FirebaseDatabase.getInstance().getReference().child("users").child(uid);
+
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        View headerView=navigationView.getHeaderView(0);
+
+        //gestione della schermata relativa all'UTENTE CORRENTE
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                //impostazione del nome dell'utente e del tipo di utente (studete/professore)
+                TextView nameText=(TextView)findViewById(R.id.Nomeprofilo);
+                TextView typeText=(TextView)findViewById(R.id.TipoProfilo);
+                nameText.setText(dataSnapshot.child("name").getValue().toString());
+                boolean teacher=(boolean)dataSnapshot.child("teacher").getValue();
+                if (teacher)  typeText.setText(R.string.teacher);
+                else typeText.setText(R.string.student);
+                //definizione ed implementazione del fab in base al tipo di utente
+                FloatingActionButton fab = findViewById(R.id.fab);
+
+                if (teacher) {
+                    fab.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent addCourse=new Intent(ProfileActivity.this, CreateCourseActivity.class);
+                            startActivity (addCourse);
+                        }
+                    });
+
+                } else{
+                    fab.hide();
+                }
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
