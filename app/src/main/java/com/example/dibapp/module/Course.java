751f43f836d15da519bca36eebd0ad63f269566b
l.mongelli10 <retro.guitar4698@gmail.com>
2020-03-11 00:37:59 +0100

    Creazione classi Course e Lesson

---
+++ app/src/main/java/com/example/dibapp/module/Course.java
@@ -0,0 +1,21 @@
+package com.example.dibapp.module;
+
+public class Course {
+    public String id;
+    public String nome;
+    public String descrizione;
+    public String professore;
+    public String laurea;
+    public String chiave;
+
+    public Course(String id, String nome, String descrizione, String professore,
+                  String laurea, String chiave) {
+        this.id = id;
+        this.descrizione=descrizione;
+        this.nome=nome;
+        this.professore=professore;
+        this.laurea=laurea;
+        this.chiave=chiave;
+
+    }
+}
---
+++ app/src/main/java/com/example/dibapp/module/Lesson.java
@@ -0,0 +1,26 @@
+package com.example.dibapp.module;
+
+public class Lesson {
+
+    String id;
+    String descrizione;
+    String data;
+    String ora_i;
+    String ora_f;
+    String chiave;
+
+    public Lesson(String id, String descrizione, String data, String ora_i,
+                  String ora_f, String chiave) {
+
+        this.id = id;
+        this.descrizione=descrizione;
+        this.data=data;
+        this.ora_i=ora_i;
+        this.ora_f=ora_f;
+        this.chiave=chiave;
+
+
+
+
+        }
+}
