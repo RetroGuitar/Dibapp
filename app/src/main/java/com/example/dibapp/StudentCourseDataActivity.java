b6e92888dcfb3f1f47ca7c37424e02cbbe500bb7
l.mongelli10 <retro.guitar4698@gmail.com>
2020-03-12 20:31:02 +0100

    Registrazione ai corsi (studente)

--- app/src/main/java/com/example/dibapp/StudentCourseDataActivity.java
+++ app/src/main/java/com/example/dibapp/StudentCourseDataActivity.java
@@ -10,7 +10,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.dibapp.module.Course;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,9 +37,13 @@ public class StudentCourseDataActivity extends AppCompatActivity {
         final TextView laurea=(TextView) findViewById(R.id.pschedalaurea);
         final Button iscriviti = (Button) findViewById(R.id.iscrivitibutton);
         final EditText chiave = (EditText) findViewById(R.id.keyreg);
+        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+        String uid= user.getUid();
+        final DatabaseReference studentCourse=FirebaseDatabase.getInstance().getReference().child("student_courses").child(uid);
         Intent intent= getIntent();
-        String id=intent.getStringExtra("id");
+        final String id=intent.getStringExtra("id");
         DatabaseReference dbRef= FirebaseDatabase.getInstance().getReference().child("courses").child(id);
+
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,7 +55,7 @@ public class StudentCourseDataActivity extends AppCompatActivity {
         dbRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String name, teacher, description, degree;
+                final String name, teacher, description, degree;
                name= dataSnapshot.child("nome").getValue().toString();
                teacher=dataSnapshot.child("professore").getValue().toString();
                description=dataSnapshot.child("descrizione").getValue().toString();
@@ -64,8 +72,12 @@ public class StudentCourseDataActivity extends AppCompatActivity {
                    @Override
                    public void onClick(View v) {
                        if(chiave.getText().toString().equals(key)){
+                           Course courseref= new Course(id, name);
+                           studentCourse.child(id).setValue(courseref);
+                           Toast.makeText(StudentCourseDataActivity.this, "Registration Successful!", Toast.LENGTH_SHORT).show();
+                           finish();
                            
-                       }
+                       }else Toast.makeText(StudentCourseDataActivity.this, "Please insert the correct Key!", Toast.LENGTH_SHORT).show();
                    }
                });
 
