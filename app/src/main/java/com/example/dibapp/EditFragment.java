49c2139bb87b8eeb80b89a728d8b1c2fa304ff71
trander97-dot <trander97@gmail.com>
2020-03-12 19:48:50 +0100

    Edit Profile.

--- app/src/main/java/com/example/dibapp/EditFragment.java
+++ app/src/main/java/com/example/dibapp/EditFragment.java
@@ -1,12 +1,31 @@
 package com.example.dibapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -59,6 +78,85 @@ public class EditFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit, container, false);
+        final View view = inflater.inflate(R.layout.fragment_edit, container, false);
+        final EditText profilename = (EditText) view.findViewById(R.id.changenametext);
+        final EditText changenumber = (EditText) view.findViewById(R.id.changeregnumbertext);
+        final EditText oldpass = (EditText) view.findViewById(R.id.oldpwdtext);
+        final EditText newpass = (EditText) view.findViewById(R.id.newpwdtext);
+        Button save = (Button) view.findViewById(R.id.saveeditsbutton);
+        Button change = (Button) view.findViewById(R.id.changepasswordbutton);
+
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String Uid = user.getUid();
+        final DatabaseReference db = FirebaseDatabase.getInstance().getReference().child("users").child(Uid);
+
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!profilename.getText().toString().isEmpty()){
+                    Map<String, Object> hopperUpdates = new HashMap<>();
+                    hopperUpdates.put("name", profilename.getText().toString());
+
+                    db.updateChildren(hopperUpdates);
+                }
+                if(!changenumber.getText().toString().isEmpty()){
+                    Map<String, Object> hopperUpdates = new HashMap<>();
+                    hopperUpdates.put("matricola", changenumber.getText().toString());
+
+                    db.updateChildren(hopperUpdates);
+                }
+            }
+        });
+
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String oldpassword, newpassword;
+                oldpassword = oldpass.getText().toString();
+                newpassword = newpass.getText().toString();
+
+                if(oldpass.getText() == null){
+                  //  oldpsw.setError("Enter old Password!");
+                  //  oldpsw.requestFocus();
+                    Toast.makeText(getContext(),"Enter old password!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(newpass.length() < 6){
+                    //newpsw.setError("Password must be >= 6 characters!");
+                    //newpsw.requestFocus();
+                    Toast.makeText(getContext(),"Password must be >= 6 characters!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(),oldpassword);
+
+                user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            user.updatePassword(newpassword).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(getContext(), "Password Changed!", Toast.LENGTH_SHORT).show();
+                                        Intent i = new Intent(getActivity(), ProfileActivity.class);
+                                        startActivity(i);
+                                    }
+                                    else {
+                                        Toast.makeText(getContext(),"Password Not Changed!",Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                        else {
+                            Toast.makeText(getContext(),"Please Enter The Old Password Correctly!",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        return view;
     }
 }
